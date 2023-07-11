69e9b09740681a1a1b11e82291109a1daca35da6
Mihai Costea <macostea@users.noreply.github.com>
2023-07-11 10:45:05 +0300

    Add debug symbols for release

--- builder/docker-entrypoint.sh
+++ builder/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cargo build --release --target=aarch64-unknown-linux-gnu
+cargo build --profile=release-with-debug --target=aarch64-unknown-linux-gnu
 
 export LD_LIBRARY_PATH=/app/rootfs/usr/lib/aarch64-linux-gnu
-cargo deb --target=aarch64-unknown-linux-gnu -v
+cargo deb --profile=release-with-debug --separate-debug-symbols --target=aarch64-unknown-linux-gnu -v
